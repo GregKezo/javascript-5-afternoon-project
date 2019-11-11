c0a98ab1582ad572e3e3a8fe478fb5ff8c336df7
Greg Kezos <greg.kezos@gmail.com>
2019-11-11 12:29:30 -0700

    everything classes except that last one because I'm not sure what it's asking for

--- classes.js
+++ classes.js
@@ -30,7 +30,16 @@
 */
 
 //Code Here
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
 
+  makeWidget() {return `${this.first_name} ${this.last_name} Widget`}
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -48,6 +57,20 @@
 */
 
 //Code Here
+
+class Manager extends Employee {
+  constructor (first_name, last_name, email, age, reports){
+    super(first_name, last_name, email, age)
+    this.reports = []
+  }
+
+  hire(employee) {
+    return this.reports.push(employee)
+  }
+  fire(index) {
+    return this.reports.splice(index, 1)
+  }
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -73,6 +96,41 @@
 
 //Code Here
 
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title, bonus){
+    super(first_name, last_name, email, age, reports)
+    this.title = 'Not a manager'
+    this.bonus = 0
+  }
+
+  managerChecker(){
+    if ( this.reports.length > 100 ){
+      return this.title = 'Bestest Manager'
+    } else if ( this.reports.length > 50 && this.reports.length <= 100) {
+      return this.title = 'Manager Plus'
+    } else if ( this.reports.length > 10 && this.reports.length <= 50) {
+      return this.title = 'Manager'
+    } else if ( this.reports.length > 3 && this.reports.length <= 10) {
+      return this.title = 'Mostly Manager'
+    } else if ( this.reports.length > 0 && this.reports.length <= 3) {
+      return this.title = 'Barely Manager'
+    } else {
+      return 'Not a manager'
+    }
+  }
+
+  hire(employee) {
+    this.reports.push(employee);
+    this.managerChecker();
+    return this.reports, this.title
+  }
+
+  fire(index) {
+    this.reports.splice(index, 1)
+    this.bonus += 100
+    return this.reports, this.bonus
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -100,4 +158,35 @@
 
 //Code Here
 
+
+class Machine {
+  constructor() {
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
+
+  makeWidgets(num){
+    this.widgets_made_count += num
+    this.wear_and_tear_count = Math.floor(this.widgets_made_count / 50)
+    return this.widgets_made_count, this.wear_and_tear_count;
+  }
+
+  fixMachine() {
+    return this.needs_reboot = true;
+  }
+
+  reboot() {
+      let wt = this.wear_and_tear_count
+      let nr = this.needs_reboot
+      function rebootComplete(wt, nr){
+        wt -= 10
+        nr = false
+        return wt, nr
+      }
+      rebootComplete(wt, nr)
+      return wt, nr
+    }
+
+  }
 
